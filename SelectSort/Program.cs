b0f1b21058a37a33bf0299375d2c16f770d556d7
Simon Lee <slee1994@hotmail.com>
2017-10-19 14:05:32 +1100

    Initial commit

---
+++ SelectSort/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1, 2, 4, 9, 11, 18, 7};
+            int LowestIndex = 0;
+            int currentLowest;
+
+            while (LowestIndex < arr.Length)
+            {
+                currentLowest = LowestIndex;
+
+                for (int i = LowestIndex + 1; i < arr.Length; i++)
+                {
+                    if (arr[currentLowest] > arr[i])
+                    {
+                        currentLowest = i;
+                    }
+                }
+
+                int temp = arr[currentLowest];
+                    arr[currentLowest] = arr[LowestIndex];
+                    arr[LowestIndex] = temp;
+                    LowestIndex++;
+                }
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+
